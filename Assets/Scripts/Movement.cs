d761bd146a325c8f764a7ea9707ac2913a826917
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-06-24 22:46:58 +0300

    code refactor

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
     private float speed;
     [SerializeField]
     private float jumpPower;
+    [SerializeField]
+    private float turnSpeed;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,7 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         TakeInput();
     }
@@ -25,20 +27,22 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector3(rb.velocity.x, (jumpPower * 100) * Time.deltaTime, 0f);
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp((jumpPower * 100) * Time.deltaTime, 0f, 7f), 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(Mathf.Clamp((speed * 100) * Time.deltaTime, 0f, 7f), rb.velocity.y, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, -180f, 0f), turnSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(Mathf.Clamp((-speed * 100) * Time.deltaTime, -7f, 0f), rb.velocity.y, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), turnSpeed * Time.deltaTime);
         }
         else
         {
-            print("Not Jump");
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            rb.velocity = new Vector3((speed * 100) * Time.deltaTime, rb.velocity.y, 0f);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.velocity = new Vector3((-speed * 100) * Time.deltaTime, rb.velocity.y, 0f);
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
         }
     }
 }
